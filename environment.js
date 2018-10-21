8ac61689e277fa8a26f15827c253526409ea7727
vokamura <vcokamura@gmail.com>
2018-10-21 01:09:43 -0700

    Separate js files and create canvas

---
+++ environment.js
@@ -0,0 +1,7 @@
+let gameContainer = document.getElementById("gameContainer");
+
+//creates drawing object
+let gameCanvas = gameContainer.getContext("2d");
+
+// gameCanvas.fillstyle = "dodgerblue";
+

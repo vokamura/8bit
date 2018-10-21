29b334a55ec2662832c2a0a32a78227a9ce31885
vokamura <vcokamura@gmail.com>
2018-10-21 09:51:00 -0700

    Store name and gender in local storage

--- hero.js
+++ hero.js
@@ -1,8 +1,14 @@
-//create a hero
+//create a hero with ES6 class
 class Hero {
     constructor(name, gender){
         this.name = name;
         this.gender = gender;
+    }
+    sayName(){
+        console.log(name);
+    }
+    returnGender(){
+        console.log(gender);
     }
     moveForward(){
         console.log("Moved forward");
@@ -15,7 +21,25 @@ class Hero {
     }
 }
 
-let hero1 = new Hero("Vikki");
+let hero1 = new Hero(getName(),getGender());
+
+function getName(){
+    localStorage.setItem("name", "Vikki");
+    let name = localStorage.getItem("name");
+    return name;
+}
+
+function getGender(){
+    localStorage.setItem("gender", "female");
+    let gender = localStorage.getItem("gender");
+    return gender;
+}
+
+console.log(hero1);
+
+for (var i = 0; i < localStorage.length; i++){
+    console.log(localStorage.getItem(localStorage.key(i)));
+}
 
 //basic movement
 document.onkeydown = function(event) {
